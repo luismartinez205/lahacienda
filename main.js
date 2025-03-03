3005f00295044c3eb98eb5cc6b12a4a92e050de6
Luis Francisco Martinez <69949816+luismartinez205@users.noreply.github.com>
2025-03-02 19:55:33 -0500

    Add files via upload
    javascript

---
+++ main.js
@@ -0,0 +1,5 @@
+const navtoggle=document.querySelector(".menu");
+const navmenu=document.querySelector(".submenu");
+navtoggle.addEventListener("click",()=>{
+navmenu.classList.toggle("nav-menu-visible")
+});
