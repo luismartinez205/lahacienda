c030dd803254f278c6f2bc6aad7b2b47e15e58f2
luismartinez205 <luisfrancisco198@gmail.com>
2025-03-04 22:27:17 -0500

    cambios en mediaqueries

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const navtoggle=document.querySelector(".menu");
-const navmenu=document.querySelector(".submenu");
+const navmenu=document.querySelector(".nav-list");
 navtoggle.addEventListener("click",()=>{
 navmenu.classList.toggle("nav-menu-visible")
 });
