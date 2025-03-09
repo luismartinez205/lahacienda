2d24195fc3604285bd6201cf55a4371c809e3c2a
luismartinez205 <luisfrancisco198@gmail.com>
2025-03-09 17:45:01 -0500

    cambios footer

--- main.js
+++ main.js
@@ -3,3 +3,6 @@ const navmenu=document.querySelector(".nav-list");
 navtoggle.addEventListener("click",()=>{
 navmenu.classList.toggle("nav-menu-visible")
 });
+
+
+ const fecha=document.getElementById("year").textContent = new Date().getFullYear();
