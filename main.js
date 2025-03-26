88b940d48b5a2876d8fa014c914e634f1a0167c0
luismartinez205 <luisfrancisco198@gmail.com>
2025-03-25 20:53:18 -0500

    agregar sección de contacto en la página de contacto; implementar navegación fija y oculta al hacer scroll

--- main.js
+++ main.js
@@ -6,3 +6,23 @@ navmenu.classList.toggle("nav-menu-visible")
 
 
  const fecha=document.getElementById("year").textContent = new Date().getFullYear();
+
+ const nav = document.querySelector('.navbar');
+
+ 
+ window.addEventListener('scroll', () => {
+        if (window.scrollY > 50) {
+            nav.classList.add('fixed-nav');
+        } else {
+            nav.classList.remove('fixed-nav');
+        }
+
+        if (window.scrollY > 1500) {
+            nav.classList.add('hidden-nav');
+        } else {
+            nav.classList.remove('hidden-nav');
+        }
+        
+ });
+
+ 
