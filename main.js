2c5f35b97806d6979af85bd464a153e60b185316
luismartinez205 <luisfrancisco198@gmail.com>
2025-03-27 23:08:18 -0500

    agregar funcionalidad de carrusel de imágenes; ajustar enlaces en las páginas de productos y nosotros

--- main.js
+++ main.js
@@ -32,5 +32,22 @@ navmenu.classList.toggle("nav-menu-visible")
    document.getElementById("main-contact-form").submit();
  }
 
+ 
+const slides = document.querySelectorAll('.slide');
+const nextBtn = document.querySelector('.next-btn');
+const prevBtn = document.querySelector('.prev-btn');
+let index = 0;
+
+nextBtn.addEventListener('click', () => {
+    slides[index].classList.remove('active');
+    index = (index + 1) % slides.length;
+    slides[index].classList.add('active');
+});
+
+prevBtn.addEventListener('click', () => {
+    slides[index].classList.remove('active');
+    index = (index - 1 + slides.length) % slides.length;
+    slides[index].classList.add('active');
+});
 
  
